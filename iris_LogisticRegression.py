ea8c8add1f5e3b8227e993470508ef8cc112a762
iamsal <sheikhsalman2013@gmail.com>
2020-01-08 23:10:55 +0600

    iris fower dataset with knn and logistic regression

---
+++ iris_LogisticRegression.py
@@ -0,0 +1,16 @@
+#import dependencies
+from sklearn.datasets import load_iris
+from sklearn.linear_model import LogisticRegression
+
+#load datast and get features and targets
+iris_flower=load_iris()
+X = iris_flower.data
+y = iris_flower.target
+
+#predict
+lr=LogisticRegression(solver='liblinear',multi_class='auto')
+lr.fit(X,y)
+result=lr.predict([[2, 4, 0.1, 1], [4, 0.6, 2, 3]])
+print(result,iris_flower.target_names[result])
+
+
